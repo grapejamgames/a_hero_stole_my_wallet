ccfc654843c0260ec9eb6500cc24be9e549237ee
chris-calmatlas <chris@calmatlas.com>
2026-01-28 17:31:04 -0700

    adlibs and random statement

--- a_hero_stole_my_wallet/Prototype/Components/attribute.gd
+++ a_hero_stole_my_wallet/Prototype/Components/attribute.gd
@@ -5,10 +5,30 @@ class_name Attribute
 
 extends Node
 
+static var adlibs_plural : Array[String] = [
+	"They had {adjective} {noun}" # brown eyes - two eyes
+]
+
+static var adlibs_singular : Array[String] = [
+	"They had a {adjective} {noun}" # brown cape - one cape
+]
+
+# static var adlibs_missing_noun
+# 	"they didn't have a {noun}"
+
+# static var adlibs_binary_yes :
+# 	"They had {noun}"
+
+
+# static var adlibs_binary_no :
+# 	"They had no {noun}"
+
 @export var _noun : String
 @export var _adjectives : Array[String]
+@export var _singular : bool = true
 
 var rand_num = RandomNumberGenerator.new()
+var adlibs : Array[String]
 
 
 func _init(noun : String = "", adjectives = null) -> void:
@@ -18,6 +38,11 @@ func _init(noun : String = "", adjectives = null) -> void:
 			add_adjective(adjectives)
 		TYPE_ARRAY:
 			add_adjectives(adjectives)
+	
+	if _singular:
+		adlibs = adlibs_singular
+	else:
+		adlibs = adlibs_plural
 
 
 func add_adjective(adjective : String) -> void:
@@ -34,14 +59,18 @@ func get_adjective() -> Array:
 
 
 func get_random_adjective() -> String:
-	var rand_int = rand_num.randi_range(0, count_adjectives() - 1)
-	return _adjectives[rand_int]
-
-
-func count_adjectives() -> int:
-	return _adjectives.size()
-
+	if _adjectives:
+		var rand_int = rand_num.randi_range(0, _adjectives.size() - 1)
+		return _adjectives[rand_int]
+	return ""
 
 func get_dictionary() -> Dictionary:
 	return {_noun : _adjectives}
 
+
+func get_random_statement(adjective : String) -> String:
+	var rand_int : int = rand_num.randi_range(0, adlibs.size() - 1)
+	var random_adlib : String = adlibs[rand_int]
+	var statement = random_adlib.format({_noun : adjective})
+	print(statement)
+	return statement
